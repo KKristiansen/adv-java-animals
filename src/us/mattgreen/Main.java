8abc283c229045b37aec71a6dd79486a6220cc33
Kristopher Kristiansen <KKristiansen@my.wctc.edu>
2020-01-29 15:16:28 -0600

    Testing update

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -1,6 +1,7 @@
 package us.mattgreen;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,12 +10,13 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Talkable> zoo = new ArrayList<>();
-
         // Lines to Replace Begin Here
-        zoo.add(new Dog(true, "Bean"));
-        zoo.add(new Cat(9, "Charlie"));
-        zoo.add(new Teacher(44, "Stacy Read"));
+        //zoo.add(new Dog(true, "Bean"));
+        //zoo.add(new Cat(9, "Charlie"));
+        //zoo.add(new Teacher(44, "Stacy Read"));
+        System.out.println("");
         // End Lines to Replace
+
 
         for (Talkable thing : zoo) {
             printOut(thing);
